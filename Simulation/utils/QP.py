dba17a7f1df336b92d0dca3f008482296981d9aa
tjards <travis.jardine@gmail.com>
2020-10-25 20:19:07 -0400

    building QP, going 3D

--- Simulation/utils/QP.py
+++ Simulation/utils/QP.py
@@ -8,104 +8,59 @@ Created on Wed Oct 21 20:19:56 2020
 import numpy as np
 from scipy import optimize as opt
 from matplotlib import pyplot as plt
-from matplotlib.animation import FuncAnimation
-import sys 
+from matplotlib.animation import FuncAnimation 
 
-
-
-
+# computes the centerpoint of the constraint
+# ------------------------------------------
 def computeXp(x,xo,r,ro,rb):
         
-        # compute distance between the two
-        d = np.linalg.norm(x-xo)
-        # find xp
-        
-        xp = xo + np.multiply(np.divide(ro+r+rb,d),(x-xo))
-        #xp = xo + np.multiply(buffer,(x-xo))
-        
+        d = np.linalg.norm(x-xo)                            # distance between obs and veh    
+        xp = xo + np.multiply(np.divide(ro+r+rb,d),(x-xo))  # compute centerpoint
+
         return xp, d
 
-    
+
+# find an optimal point around obstacle 
+# -------------------------------------    
 def runOpt(x, xt, xp, A, b):
     
-
     cons = {'type':'ineq',
             'fun':lambda x: b - np.dot(A,x),
             'jac':lambda x: -A}
-           
-   # x0 = np.array([0, 2.5])
-    x0 = x
-    
+               
     # unconstrained case
-    ux = opt.minimize(f, x0, constraints=None, args = [xt[0], xt[1]])
-    # constrained case
-    #cx = opt.minimize(f, x0, bounds=bnds, constraints=cons)
-    cx = opt.minimize(f, x0, constraints=cons, args = [xt[0], xt[1]])
+    # ux = opt.minimize(f, x, constraints=None, args = [xt[0], xt[1]])
     
-    return ux, cx, A, b
+    # constrained case
+    cx = opt.minimize(f, x, constraints=cons, args = [xt[0], xt[1]])
+    #cx = opt.minimize(f, x0, bounds=bnds, constraints=cons)
 
+    return cx
+
+
+# the objective function
+# ----------------------
 def f(x, xt):
     
-    #dx = x - xt  
-    #return -(2*x[0]*x[1] + 2*x[0] - x[0]**2 - 2*x[1]**2)
     return ((x[0]-xt[0])**2 + (x[1]-xt[1])**2)
 
 
+# animate the obstacle avoidance
+# -----------------------------
+def myAnimation(myData_QP):
 
-def myAnimation(xv,xo,xt,A,b,r,ro,rb):
-  
-    # def computeXp(x,xo,r,ro,rb):
-        
-    #     # compute distance between the two
-    #     d = np.linalg.norm(x-xo)
-    #     # find xp
-        
-    #     xp = xo + np.multiply(np.divide(ro+r+rb,d),(x-xo))
-    #     #xp = xo + np.multiply(buffer,(x-xo))
-        
-    #     return xp, d
-    
-    # def runOpt(x, xt, xp):
-        
-    #     # A = np.array([[ 1., 1.],
-    #     #               [-1., 2.],
-    #     #               [-1., 0.],
-    #     #               [0., -1.],
-    #     #               [0.,  1.]])
-        
-    #     A = np.zeros([1,2])
-    #     A[0,:] = xo - xp
-        
-    #     #b = np.array([7., 4., 0., 0., 4.])
-    #     b = np.dot(xp,(xo-xp))
-        
-        
-        
-    #     cons = {'type':'ineq',
-    #             'fun':lambda x: b - np.dot(A,x),
-    #             'jac':lambda x: -A}
-               
-    #    # x0 = np.array([0, 2.5])
-    #     x0 = x
-        
-    #     # unconstrained case
-    #     ux = opt.minimize(f, x0, constraints=None, args = [xt[0], xt[1]])
-    #     # constrained case
-    #     #cx = opt.minimize(f, x0, bounds=bnds, constraints=cons)
-    #     cx = opt.minimize(f, x0, constraints=cons, args = [xt[0], xt[1]])
-        
-    #     return ux, cx, A, b
-    
-    # def f(x, xt):
-        
-    #     #dx = x - xt  
-    #     #return -(2*x[0]*x[1] + 2*x[0] - x[0]**2 - 2*x[1]**2)
-    #     return ((x[0]-xt[0])**2 + (x[1]-xt[1])**2)
-    
-    #%% Initialize Parameters 
-    
+    # initialize data store 
+    # ---------------------
+    xv = myData_QP[0,0:2] 
+    xo = myData_QP[0,2:4] 
+    xt = myData_QP[0,4:6] 
+    A = myData_QP[0,6:8] 
+    b = myData_QP[0,8:9] 
+    ux = myData_QP[0,9:11] 
+    cx = myData_QP[0,11:13]
 
-    
+    # initiate the figure
+    # -------------------
     fig, ax = plt.subplots()
     plotSize = 5
     x = np.linspace(-plotSize, plotSize, 100)
@@ -113,68 +68,69 @@ def myAnimation(xv,xo,xt,A,b,r,ro,rb):
     X, Y = np.meshgrid(x, y)
     Z = f(np.vstack([X.ravel(), Y.ravel()]), xt).reshape((100,100))
     ax.contour(X, Y, Z, np.arange(-2,20, 1), alpha=0.3)
-    # initialize lines that need updating 
-    line_obs = ax.scatter(xo[0], xo[1], s=4000*ro, marker = 'o', alpha = 0.2, color = 'blue')
-    line_veh = ax.scatter(xv[0], xv[1], s=4000*ro, marker = 'o', alpha = 0.2, color = 'green')
-    line_tar = ax.scatter(xt[0], xt[1], s=4000*ro, marker = 'o', alpha = 0.2, color = 'black')
-    line_ux = ax.scatter(xt[0], xt[1], s=1000*ro, marker = 'x', alpha = 1, color = 'black')
-    line_cx = ax.scatter(xt[0], xt[1], s=1000*ro, marker = 'x', alpha = 1, color = 'red')
+    
+    # initialize lines that need updating
+    # ------------------------------------
+    line_obs = ax.scatter(xo[0], xo[1], s=400, marker = 'o', alpha = 0.2, color = 'blue')
+    line_veh = ax.scatter(xv[0], xv[1], s=400, marker = 'o', alpha = 0.2, color = 'green')
+    line_tar = ax.scatter(xt[0], xt[1], s=400, marker = 'o', alpha = 0.2, color = 'black')
+    line_ux = ax.scatter(xt[0], xt[1], s=100, marker = 'x', alpha = 1, color = 'black')
+    line_cx = ax.scatter(xt[0], xt[1], s=100, marker = 'x', alpha = 1, color = 'red')
     line_xv2cx, = ax.plot([xv[0],xt[0]],[xv[1],xt[1]], 'g:', linewidth=1)
     line_xt2cx, = ax.plot([xt[0],xt[0]],[xt[1],xt[1]], 'b:', linewidth=1)
-    #A = np.zeros([1,2])
-    #b = 0
-    yy = -np.divide(A[0,0],A[0,1])*x+np.divide(b,A[0,1])
+
+    # %% this is the constraint illustration
+    # --------------------------------------
+    #yy = -np.divide(A[0,0],A[0,1])*x+np.divide(b,A[0,1])
+    yy = -np.divide(A[0],A[1])*x+np.divide(b,A[1])
     line_const, = ax.plot(x, yy, 'r:', linewidth=1)
+    
     plt.axis([-plotSize,plotSize,-plotSize,plotSize])
     plt.xlabel('x-direction')
     plt.ylabel('y-direction')
     plt.title('obstacle avoid')
-    plt.show()
+    #plt.show()
     
     
     def update(i):
       
-        #%% Move 
-        xv[0] = xv[0] + 0.1*i*0.2
-        xv[1] = xv[1] + 0.3
-        if i > 8:    
-            xv[1] = xv[1] - 0.1
-    
-        # my new ones
+        #%% extract data
+        # --------------
+        xv = myData_QP[i,0:2] 
+        xo = myData_QP[i,2:4] 
+        xt = myData_QP[i,4:6] 
+        A = myData_QP[i,6:8] 
+        b = myData_QP[i,8:9] 
+        ux = myData_QP[i,9:11] 
+        cx = myData_QP[i,11:13]
+        
+        #%% update the lines
+        # -----------------
         line_obs.set_offsets([xo[0],xo[1]])
         line_veh.set_offsets([xv[0],xv[1]])
         line_tar.set_offsets([xt[0],xt[1]])
-    
-    
-        #%% Compute point on edge of obstacle 
-        
-        xp, d = computeXp(xv,xo,r,ro,rb)
-        
-        #update constraints 
-        A = np.zeros([1,2])
-        A[0,:] = xo - xp
-        b = np.dot(xp,(xo-xp))
-        
-        #%% Run the optimization 
-        
-        ux, cx, A, b = runOpt(xv, xt, xp, A, b)
-        
-        # my new one
-        line_ux.set_offsets([ux['x'][0], ux['x'][1]])
-        line_cx.set_offsets([cx['x'][0], cx['x'][1]])
-        line_xv2cx.set_xdata([xv[0],cx['x'][0]])
-        line_xv2cx.set_ydata([xv[1],cx['x'][1]])
-        line_xt2cx.set_xdata([xt[0],cx['x'][0]])
-        line_xt2cx.set_ydata([xt[1],cx['x'][1]])
-        
-        yy = -np.divide(A[0,0],A[0,1])*x+np.divide(b,A[0,1])
+        line_ux.set_offsets([ux[0], ux[1]])
+        line_cx.set_offsets([cx[0], cx[1]])
+        line_xv2cx.set_xdata([xv[0],cx[0]])
+        line_xv2cx.set_ydata([xv[1],cx[1]])
+        line_xt2cx.set_xdata([xt[0],cx[0]])
+        line_xt2cx.set_ydata([xt[1],cx[1]])
+        #yy = -np.divide(A[0,0],A[0,1])*x+np.divide(b,A[0,1])
+        yy = -np.divide(A[0],A[1])*x+np.divide(b,A[1])
         line_const.set_xdata(x)
         line_const.set_ydata(yy)    
         
     
-    
+    #%% build the animation
+    # ---------------------
     anim = FuncAnimation(fig, update, frames=np.arange(0, 20), interval=200, blit=False)
     anim.save('test.gif', writer='ffmpeg') #my add
+
+
+
+
+
+
 
 #%% LEGACY stuff
 
--- Simulation/utils/QP_main.py
+++ Simulation/utils/QP_main.py
@@ -9,30 +9,56 @@ import QP
 import numpy as np
 
 
+#%% Setup
+nTrials = 20
+ro      = 0.1   # obstacle safety buffer
+r       = 0.1   # vehicle safety buffer
+rb      = 0.3   # addition buffer
+rb = np.minimum(1-(ro+r),rb)    # total buffer must be < 1
 
-
+# initial positions
 xv = np.array([-3.7, -4.7])   # vehicle position
-xo = np.array([-1, -1.5])  # obstacle position
-xt = np.array([0,0])  # target position 
-ro = 0.1
-r = 0.1
-rb = 0.3
-#buffer = 0.3
+xo = np.array([-1, -1.5])     # obstacle position
+xt = np.array([0,0])          # target position 
 
+#%% Initialize 
 
-xp, d = QP.computeXp(xv,xo,r,ro,rb)
-
-# A = np.array([[ 1., 1.],
-#               [-1., 2.],
-#               [-1., 0.],
-#               [0., -1.],
-#               [0.,  1.]])
-
-A = np.zeros([1,2])
+xp, d = QP.computeXp(xv,xo,r,ro,rb) # compute constraint centerpoint 
+A = np.zeros([1,2])                 # inequalities
 A[0,:] = xo - xp
-
-#b = np.array([7., 4., 0., 0., 4.])
 b = np.dot(xp,(xo-xp))
+ux = xt                             # target setpoint
+cx = xt                             # target setpoint 
+myData_QP = np.zeros([nTrials, 13]) # data store
 
+# move the vehicles and/or obstacle around a bit
+for i in range(0,nTrials):
 
-QP.myAnimation(xv,xo,xt,A,b,r,ro,rb)
+    # pseudo-random movement 
+    xv[0] = xv[0] + 0.1*i*0.2
+    xv[1] = xv[1] + 0.3
+    if i > 8:    
+        xv[1] = xv[1] - 0.1
+     
+    # Update constraints     
+    xp, d = QP.computeXp(xv,xo,r,ro,rb) 
+    A[0,:] = xo - xp
+    b = np.dot(xp,(xo-xp))
+    
+    # find new optimal point 
+    cx = QP.runOpt(xv, xt, xp, A, b)
+    ux = cx
+
+    # update data store
+    myData_QP[i,0:2] = np.array(xv)
+    myData_QP[i,2:4] = np.array(xo)
+    myData_QP[i,4:6] = np.array(xt)
+    myData_QP[i,6:8] = np.array(A)
+    myData_QP[i,8:9] = np.array(b)
+    myData_QP[i,9:11] = np.array(ux['x'][:])    # unconstrained case (delete later to save time)
+    myData_QP[i,11:13] = np.array(cx['x'][:])   # constrained case
+    
+    # we would feed these new points into here
+    # ---> feed cx here
+ 
+QP.myAnimation(myData_QP)
